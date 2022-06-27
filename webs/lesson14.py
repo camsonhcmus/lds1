68502658fee95f4fba9f9e5b222ed32eaf1230c5
camsonhcmus <105847165+camsonhcmus@users.noreply.github.com>
2022-06-27 15:23:48 +0700

    Update lesson14.py

--- webs/lesson14.py
+++ webs/lesson14.py
@@ -18,46 +18,104 @@ def app():
 
 	st.markdown('<center><p class="big-font"><font color="darkblue">Bài 14: Xử lý ngoại lệ</center></p>', unsafe_allow_html=True)
 
-	st.write("### Hãy hoàn thành câu sau với các lựa chọn bên dưới:")
+	st.write("### Hãy hoàn thành câu hỏi liên quan đến các hình dưới:")
 
-	st.markdown('<center><span style="font-size: 40px"><strong><span class="css-10trblm e16nr0p30">Chúng ta đều bắt đầu học tiểu học ở lớp ...</span></span></center>', unsafe_allow_html=True)
+	#st.markdown('<center><span style="font-size: 40px"><strong><span class="css-10trblm e16nr0p30">Chúng ta đều bắt đầu học tiểu học ở lớp ...</span></span></center>', unsafe_allow_html=True)
 		
 
 	col1, col2, col3 = st.columns(3)
 
 	with col1:
 		st.image(Image.open('./picture/cow.jpg'), width=400)
-		st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> con bò </center></span></div>', unsafe_allow_html=True)
+		#st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> con bò </center></span></div>', unsafe_allow_html=True)
+		cow = st.checkbox("Hiện câu hỏi liên quan tới con bò")
+		if cow:
+			cowques = st.radio("Con bò đứng trên ...", options=["Ngôi nhà", "Phòng Học", "Đồng cỏ", "Cái cây"])
+
+			if cowques == "Đồng cỏ":
+				
+				st.write("#### Chúc mừng :tada:")
+			else:
+				st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
+		else:
+			pass
 
 	with col2:
 		st.image(Image.open('./picture/boat.jpg'), width=400)
-		st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> thuyền </center></span></div>', unsafe_allow_html=True)
+		#st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> thuyền </center></span></div>', unsafe_allow_html=True)
+		boats = st.checkbox("Hiện câu hỏi liên quan tới chiếc thuyền")
+		if boats:
+			cowques = st.radio("Chiếc thuyền đi trên ...", options=["Sàn nhà", "Lớp Học", "Đồng cỏ", "Biển"])
+
+			if cowques == "Biển":
+				
+				st.write("#### Chúc mừng :tada:")
+			else:
+				st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
+		else:
+			pass
 
 	with col3:
 		st.image(Image.open('./picture/chair.jpg'), width=350)
-		st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 400px;"><span style="font-size: 20px"><center><strong> ghế dựa </center></span></div>', unsafe_allow_html=True)
+		#st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 400px;"><span style="font-size: 20px"><center><strong> ghế dựa </center></span></div>', unsafe_allow_html=True)
+		chair = st.checkbox("Hiện câu hỏi liên quan tới cái ghế")
+
+		if chair:
+			cowques = st.radio("Chiếc ghế trên có bao nhiêu tay vịn ?", options=["bốn", "Lớp Học", "2", "Tất cả các ý kiến trên"])
+
+			try:
+				int(cowques)
+				st.write("#### Chúc mừng :tada:")
+			except:
+				st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
+		else:
+			pass
 
 	col4, col5, col6 = st.columns(3)
 
 	with col4:
 		st.image(Image.open('./picture/tree.jpg'), width=400)
-		st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> cây </center></span></div>', unsafe_allow_html=True)
+		#st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> cây </center></span></div>', unsafe_allow_html=True)
+		trees = st.checkbox("Hiện câu hỏi liên quan tới cái cây")
+
+		if trees:
+			onetrees = st.radio("Cái cây màu gì ?", options=["Đỏ", "Xanh lá", "Tím", "Vàng"])
+
+			if onetrees == "Xanh lá":
+				
+				st.write("#### Chúc mừng :tada:")
+			else:
+				st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
+		else:
+			pass
 
 	with col5:
 		st.image(Image.open('./picture/number1.jpg'), width=400)
-		st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> số 1 </center></span></div>', unsafe_allow_html=True)
+		#st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 500px;"><span style="font-size: 20px"><center><strong> số 1 </center></span></div>', unsafe_allow_html=True)
+		s1 =st.checkbox("Hiện câu hỏi liên quan tới số 1")
+		if s1:
+			orig = st.radio("Chúng ta đều bắt đầu học tiểu học ở lớp ...", options=["con bò", "cây", "1", "học sinh"])
 
+			try:
+				int(orig)
+				st.write("#### Chúc mừng :tada:")
+			except:
+				st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
+		else:
+			pass
 
 	with col6:
-	 	st.image(Image.open('./picture/student.jpg'), width=600)
-	 	st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 700px;"><span style="font-size: 20px"><center><strong> học sinh </center></span></div>', unsafe_allow_html=True)
+		st.image(Image.open('./picture/student.jpg'), width=600)
+		#st.markdown('<div data-testid="caption" class="css-1b0udgb etr89bj0" style="width: 700px;"><span style="font-size: 20px"><center><strong> học sinh </center></span></div>', unsafe_allow_html=True)
+		stu =st.checkbox("Hiện câu hỏi liên quan tới học sinh")
 
+		if stu:
+			onestu = st.radio("Ngoài sách vở cái gì cũng quan trọng với học sinh ?", options=["Kiến thức", "Bằng cấp", "Bạn bè", "Tất cả các ý trên"])
 
-	orig = st.radio("Hãy lựa chọn phương án phù hợp", options=["con bò", "thuyền", "ghế dựa", "cây", "1", "học sinh"])
-
-	try:
-		int(orig)
-		st.write("## Chúc mừng :tada:")
-	except:
-		st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
-
+			if onestu == "Tất cả các ý trên":
+				
+				st.write("#### Chúc mừng :tada:")
+			else:
+				st.error("Hãy chọn lại phương án phù hợp bạn nhé :smile:")
+		else:
+			pass
