379836fb260ed12e6aa378a8fb1c018e39ab4dfd
camsonhcmus <105847165+camsonhcmus@users.noreply.github.com>
2022-06-28 16:10:23 +0700

    Update lesson7.py

--- webs/lesson7.py
+++ webs/lesson7.py
@@ -21,9 +21,9 @@ def app():
 
 	st.markdown('<center><p class="big-font"><font color="darkblue">Bài 7: Datetime</center></p>', unsafe_allow_html=True)
 
-	st.write("## Hãy nhập thời gian hiện tại vào robot")
+	st.write("## Hãy chọn ngày sinh nhật")
 
-	st.image(Image.open('./picture/robot.png'), width=250)
+	st.image(Image.open('./picture/cake.jpg'), width=400)
 
 	st.write("## Ngày: ")
 
@@ -33,67 +33,18 @@ def app():
 
 	month = st.selectbox(" ", options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
-	st.write("## Năm: ")
-	
-	year = st.radio(" ", options=[2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025], horizontal=True)
+	tim = datetime.now().timetuple()
+	a1 = str("%s/%s/%s"%(tim[1], tim[2], tim[0]))
+	b1 = str("%s/%s/%s"%(month, day, tim[0]))
+	date_format = "%m/%d/%Y"
+	a = datetime.strptime(a1, date_format)
+	b = datetime.strptime(b1, date_format)
+	delta = b - a
 
-	st.write(" ")
-
-	def convert(month):
-		if month == 1:
-			return "Jan"
-		elif month == 2:
-			return "Feb"
-		elif month == 3:
-			return "Mar"
-		elif month == 4:
-			return "Apr"
-		elif month == 5:
-			return "May"
-		elif month == 6:
-			return "Jun"
-		elif month == 7:
-			return "July"
-		elif month == 8:
-			return "Aug"
-		elif month == 9:
-			return "Sep"
-		elif month == 10:
-			return "Oct"
-		elif month == 11:
-			return "Nov"
-		else:
-			return "Dec"
-
-	def convert_day(day):
-		if day == 0:
-			return "Thứ hai"
-		if day == 1:
-			return "Thứ ba"
-		if day == 2:
-			return "Thứ tư"
-		if day == 3:
-			return "Thứ năm"
-		if day == 4:
-			return "Thứ sáu"
-		if day == 5:
-			return "Thứ bảy"
-		if day == 6:
-			return "Chủ nhật"
-       
-
-
-	if st.button("Nhập thông tin: "):
-
-		ti = time.asctime(time.localtime(time.time()))
-		spl_ti = ti.split(" ")
-		currentdate = datetime.now().timetuple()
-		days = calendar.weekday(currentdate[0], currentdate[1], currentdate[2])
-
-
-		if day==int(spl_ti[2]) and convert(month)==spl_ti[1] and year==int(spl_ti[4]):
-			st.write("Cảm ơn bạn đã nhập thông tin chính xác :smile:")
-			st.write("Thời gian hiện tại là: %s, ngày %s tháng %s năm %s" % (convert_day(days), currentdate[2], currentdate[1], currentdate[0]))
-		else:
-			st.write("Thông tin của bạn đã sai :cry: vì:")
-			st.write("Thời gian hiện tại không phải: %s, ngày %s tháng %s năm %s" % (convert_day(calendar.weekday(year, month, day)), day, month, year))
+	if st.button("Xác nhận"):
+		if delta.days > 0:
+			st.write("#### Còn %s ngày nữa là tới sinh nhật bạn"%(delta.days))
+		elif delta.days == 0:
+			st.write("#### Chúc mừng sinh nhật bạn")			
+		elif delta.days < 0:
+			st.write("#### Sinh nhật bạn đã qua %s ngày"%(delta.days * -1))
