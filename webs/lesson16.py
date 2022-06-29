d043c22d927aab26b2ab97751dbb76722c70da2d
camsonhcmus <105847165+camsonhcmus@users.noreply.github.com>
2022-06-29 16:57:01 +0700

    Update lesson16.py

--- webs/lesson16.py
+++ webs/lesson16.py
@@ -17,7 +17,7 @@ def app():
 	</style>
 	""", unsafe_allow_html=True)
 
-	st.markdown('<center><p class="big-font"><font color="darkblue">Bài 16: Dictionary (mở rộng)</center></p>', unsafe_allow_html=True)
+	st.markdown('<center><p class="big-font"><font color="darkblue">Bài 13: Dictionary (mở rộng)</center></p>', unsafe_allow_html=True)
 
 	st.write("### Hãy giúp mình xắp xếp lịch học hè (1 ngày 5 môn ):")
 
