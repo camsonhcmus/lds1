753843d5b61b1d1bc731afca4b092b290e6da1aa
camsonhcmus <105847165+camsonhcmus@users.noreply.github.com>
2022-06-30 13:37:25 +0700

    Update lesson18.py

--- webs/lesson18.py
+++ webs/lesson18.py
@@ -23,7 +23,7 @@ def app():
 	st.markdown("## Hãy xắp xếp hình theo hình mẫu")
 	st.image(Image.open('./picture/van.jpg'), width=800)
 	
-	st.write("### Hình cần xắp xếp: ")
+	st.write("### Hình cần sắp xếp: ")
 	col1, col2, col3, col4 = st.columns(4)
 
 	st.markdown('''''', unsafe_allow_html=True)
