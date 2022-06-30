38dd76b662f8d6674d3e8ce52718e486acea2b97
camsonhcmus <105847165+camsonhcmus@users.noreply.github.com>
2022-06-30 15:48:21 +0700

    Update lesson7.py

--- webs/lesson7.py
+++ webs/lesson7.py
@@ -30,7 +30,7 @@ def app():
 	st.write("## Ngày: ")
 
 	#day = st.slider(" ", 1, 31, 1)
-	day = st.selectbox(" ", options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+	day = st.selectbox(" ", options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31])
 
 	st.write("## Tháng: ")
 
